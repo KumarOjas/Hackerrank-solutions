618c88ff05b74bd809a5b188914dbd93637322cd
Kumar Ojas <44812427+KumarOjas@users.noreply.github.com>
2020-07-12 00:23:28 +0530

    Create Simplearraysum.c

---
+++ Simplearraysum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main ()
+{
+int sum=0;
+int n;
+scanf("%d",&n);
+int arr[n];
+int i;
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+    sum=sum+arr[i];
+}
+
+ printf("%d",sum);
+
+}
